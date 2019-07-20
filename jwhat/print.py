efde4a0ec367a8478f24a1f0ce2ead06844f8932
Robin Lange <robin.langenc@gmail.com>
2019-07-20 12:16:04 +1000

    Refactoring of context passing (with proper dictionaries)

--- jwhat/print.py
+++ jwhat/print.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 from termcolor import cprint, colored
 from .primitives import is_primitive
 from .CollapsedData import CollapsedItem
@@ -8,62 +9,89 @@ CHAR_BRANCH_BOTTOM = "┬"
 CHAR_BRANCH_LEFT = "├"
 CHAR_ANGLE = "└"
 
-def print_data(config, collapsed_data, current_depth=0, current_delta=0, parents_deltas=[], compact=False, last=False, color_picker=None):
+def print_data(config, collapsed_data, context=None):
 
-    if color_picker is None:
-        color_picker = ColorPicker()
+    if context is None:
+        context = _make_initial_context()
 
-    parent_bars_txt = _get_parents_bars_txt(parents_deltas, config["no_color"])
-    collapsed_end_newline = "\n" + parent_bars_txt if last or not compact else ""
+    parent_bars_txt = _get_parents_bars_txt(context["parents_deltas"], config["no_color"])
+    collapsed_end_newline = "\n" + parent_bars_txt if context["last"] or not context["compact"] else ""
 
     if isinstance(collapsed_data, CollapsedItem):
         print(": " + str(collapsed_data) + collapsed_end_newline)
         return
 
-    if config["max_depth"] != -1 and current_depth == config["max_depth"]:
+    if config["max_depth"] != -1 and context["current_depth"] == config["max_depth"]:
         text = colored_wrapper(": <max depth reached>", "white", config["no_color"]) + collapsed_end_newline
         print(text)
         return
 
     if isinstance(collapsed_data, dict):
 
-        color = color_picker.get_color()
+        color = context["color_picker"].get_color()
         nb_items = len(collapsed_data)
         max_key_len = max(map(len, collapsed_data.keys()))
-        are_all_collapsed_items = all(map(lambda it: isinstance(it, CollapsedItem), collapsed_data.values()))
-        new_compact = are_all_collapsed_items
+        are_all_items_collapsed = all(map(lambda it: isinstance(it, CollapsedItem), collapsed_data.values()))
 
         for i, (key, item) in enumerate(collapsed_data.items()):
-            _print_data_with_key(config, key, item, current_depth, current_delta, color, i, nb_items, max_key_len, parents_deltas, new_compact, last, color_picker)
+
+            iterable_context = {
+                "key_string": key,
+                "color": color,
+                "index": i,
+                "nb_items": nb_items,
+                "max_key_len": max_key_len,
+                "are_all_items_collapsed": are_all_items_collapsed
+            }
+
+            _print_data_with_key(config, item, iterable_context, context)
 
     elif isinstance(collapsed_data, list):
 
-        color = color_picker.get_color()
+        color = context["color_picker"].get_color()
         nb_items = len(collapsed_data)
 
         keys_list = ["{%d}" % i for i in range(nb_items)]
         max_key_len = max(map(len, keys_list))
-        are_all_collapsed_items = all(map(lambda it: isinstance(it, CollapsedItem), collapsed_data))
-        new_compact = are_all_collapsed_items
+        are_all_items_collapsed = all(map(lambda it: isinstance(it, CollapsedItem), collapsed_data))
 
         for i, (key, item) in enumerate(zip(keys_list, collapsed_data)):
-            _print_data_with_key(config, key, item, current_depth, current_delta, color, i, nb_items, max_key_len, parents_deltas, new_compact, last, color_picker)
+
+            iterable_context = {
+                "key_string": key,
+                "color": color,
+                "index": i,
+                "nb_items": nb_items,
+                "max_key_len": max_key_len,
+                "are_all_items_collapsed": are_all_items_collapsed
+            }
+    
+            _print_data_with_key(config, item, iterable_context, context)
 
 
-def _print_data_with_key(config, key, item, current_depth, current_delta, color, i, nb_items, max_key_len, parents_deltas, compact, last, color_picker):
+def _print_data_with_key(config, item, iterable_context, context):
 
-    formatted_key_txt = _get_formatted_key(config, key, current_delta, color, i, nb_items, max_key_len, parents_deltas)
+    formatted_key_txt = _get_formatted_key(config, iterable_context, context)
     print(formatted_key_txt, end="")
 
-    last = i == nb_items - 1
-    new_delta = max_key_len
-    new_parent_color = "" if last else color
-    new_parents_deltas = parents_deltas + [(current_delta, new_parent_color)]
-    new_depth = current_depth + 1
+    new_context = deepcopy(context)
 
-    print_data(config, item, new_depth, new_delta, new_parents_deltas, compact=compact, last=last, color_picker=color_picker)
+    new_context["last"] = iterable_context["index"] == iterable_context["nb_items"] - 1
+    new_parent_color = "" if new_context["last"] else iterable_context["color"]
+    new_context["current_delta"] = iterable_context["max_key_len"]
+    new_context["parents_deltas"] = context["parents_deltas"] + [(context["current_delta"], new_parent_color)]
+    new_context["current_depth"] = context["current_depth"] + 1
+    new_context["compact"] = iterable_context["are_all_items_collapsed"]
 
-def _get_formatted_key(config, key, current_delta, color, index, nb_items, max_key_len, parents_deltas):
+    print_data(config, item, new_context)
+
+def _get_formatted_key(config, iterable_context, context):
+
+    index = iterable_context["index"]
+    max_key_len = iterable_context["max_key_len"]
+    key = iterable_context["key_string"]
+    color = iterable_context["color"]
+    nb_items = iterable_context["nb_items"]
 
     if index == 0:
 
@@ -74,9 +102,9 @@ def _get_formatted_key(config, key, current_delta, color, index, nb_items, max_k
 
     else:
 
-        parent_bars_txt = _get_parents_bars_txt(parents_deltas, config["no_color"])
+        parent_bars_txt = _get_parents_bars_txt(context["parents_deltas"], config["no_color"])
 
-        space_padding = " " * current_delta
+        space_padding = " " * context["current_delta"]
 
         first_char_branch = CHAR_ANGLE if index == nb_items - 1 else CHAR_BRANCH_LEFT
         padding_branch = (max_key_len - len(key)) * CHAR_HORIZ
@@ -95,6 +123,18 @@ def _get_parents_bars_txt(parents_deltas, no_color):
             parent_bars_txt += " " * padding + colored_wrapper(CHAR_VERT, parent_color, no_color)
 
     return parent_bars_txt
+
+def _make_initial_context():
+
+    context = {}
+    context["color_picker"] = ColorPicker()
+    context["current_depth"] = 0
+    context["current_delta"] = 0
+    context["parents_deltas"] = []
+    context["compact"] = False
+    context["last"] = False
+
+    return context
 
 def colored_wrapper(text, color, no_color):
     if no_color:
